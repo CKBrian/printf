5ca79e06e7740f2b9239824099860b2c495f9f7c
TimoEle <tinjokuria@gmail.com>
2023-07-23 18:36:57 +0300

    Updated the header file

--- main.h
+++ main.h
@@ -4,8 +4,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+
 char *to_strn(int n);
+
 int _strlen(char *s);
+
 int _printf(const char *format, ...);
+
 typedef void (*specifier_handler)(va_list);
+
+struct specifier_map
+{
+	char specifier;
+	specifier_handler handler;
+};
+
+void handler_char(va_list arg);
+void handler_string(va_list arg);
+void handler_integer(va_list arg);
+
+struct specifier_map specifiers[] = 
+{
+	{'c', handler_char},
+	{'s', handler_string},
+	{'i', handler_integer},
+	{'d', handler_integer},
+};
+
 #endif /* MAIN_H */
