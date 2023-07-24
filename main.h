5d788c8c5d61aed7d75df2956ae3d7811c7db52e
Brian <chumobrian@gmail.com>
2023-07-24 19:15:46 +0000

    chore(main.h): Update main.h file with all prototypes

--- main.h
+++ main.h
@@ -5,30 +5,45 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-char *to_strn(int n);
-
+int (*get_funct(char s))(va_list arg);
+char *to_strn(long int n);
+char *unInt_to_strn(long int n);
+char *to_binary(long int n);
+char *to_octal(long int n);
+char *to_HEX(long int n);
+char *to_hex(long int n);
 int _strlen(char *s);
-
 int _printf(const char *format, ...);
-
-typedef void (*specifier_handler)(va_list);
 
 /**
  * struct specifier_map - Mapping specifiers to their functions
  * @specifier: the map itself
- * @handler: function pointer 
- *
+ * @handler: function pointer
  */
-
-struct specifier_map
+typedef struct specifier_map
 {
 	char specifier;
-	specifier_handler handler;
-};
+	int (*handler)(va_list);
+} specifier_map;
 
-void handler_char(va_list arg);
-void handler_string(va_list arg);
-void handler_integer(va_list arg);
+/**
+ * struct hexa - mapping decimal (base 10) to hexadecimal letter
+ * @num: the map to hexadecimal letters (A, B ...)
+ * @h: hexadecimal letters (A, B, ...)
+ */
+typedef struct hexa
+{
+	int num;
+	char h;
+} hex_d;
 
+int handler_char(va_list arg);
+int handler_string(va_list arg);
+int handler_integer(va_list arg);
+int handler_unInteger(va_list arg);
+int handler_binary(va_list arg);
+int handler_octal(va_list arg);
+int handler_HEX(va_list arg);
+int handler_hex(va_list arg);
 
 #endif /* MAIN_H */
