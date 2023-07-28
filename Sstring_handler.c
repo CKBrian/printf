30fb68a6cb8d70a0e55ae2236018ff3e9c8119d2
Brian <chumobrian@gmail.com>
2023-07-28 14:55:19 +0300

    fix: Fix %S segfault and reduce overhead by using bitwise to convert number to hexadecimal

--- Sstring_handler.c
+++ Sstring_handler.c
@@ -1,42 +1,40 @@
 #include "main.h"
+
 /**
- * write_Sstring - Handling function for %S specifier
- * @arg: va_list containing the arguments
- * Return: Length of the output string
+ * write_Sstring - Entry point
+ * Description: Handling function for strings
+ * @arg: argument passed
+ * Return: Output string length
  */
+
 int write_Sstring(va_list arg)
 {
 	char *str = va_arg(arg, char*);
-	int i, strlen = 0;
-	char hex[3];
+	int strlen = 0, i = 0;
+	unsigned int k;
+	char *ptr = NULL, *s = "\\x";
 
 	if (str == NULL)
 	{
-	return (write(1, "(null)", 6));
-	}
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-	if (str[i] < 32 || str[i] >= 127)
-	{
-		hex[0] = '\\';
-		if ((str[i] >> 4) < 10)
-			hex[1] = (str[i] >> 4) + '0';
-		else
-			hex[1] = (str[i] >> 4) - 10 + 'A';
-		if ((str[i] & 0x0F) < 10)
-			hex[2] = (str[i] & 0x0F) + '0';
-		else
-			hex[2] = (str[i] & 0x0F) - 10 + 'A';
-
-		strlen += write(1, hex, 3);
+		return (write(1, "(null)", 6));
 	}
 	else
 	{
-		strlen += write(1, &str[i], 1);
-	}
-	}
+		for (i = 0; *(str + i) != '\0'; i++)
+		{
+		if (*(str + i) < 32 || *(str + i) >= 127)
+		{
+			k = *(str + i);
+			strlen += write(1, s, 2);
+			if (k < 16)
+				strlen += write(1, "0", 1);
+			ptr = to_HEX(k);
+			strlen += write(1, ptr, 1);
 
+		}
+		strlen += write(1, &(*(str + i)), 1);
+		}
+	}
+	free(ptr);
 	return (strlen);
 }
-
