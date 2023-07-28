5696798ffbf1ca20988a54de324db7d624997092
Brian <chumobrian@gmail.com>
2023-07-28 10:12:52 +0300

    fix(binary_handler): Mallocs return

--- binary_handler.c
+++ binary_handler.c
@@ -8,12 +8,15 @@ int write_binary(va_list arg)
 {
 	unsigned int num = va_arg(arg, unsigned int);
 	char *str = to_binary(num);
-	int strlen = _strlen(str);
+	int strlen;
 
-	if (num == 0)
-		write(1, "0", 1);
+	if (str == NULL || num == 0)
+		strlen = write(1, "0", 1);
 	else
+	{
+		strlen = _strlen(str);
 		write(1, str, strlen);
+	}
 	free(str);
 	return (strlen);
 }
